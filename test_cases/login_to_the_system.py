9476a8995f8ec9d69d320823d4f72b1fa88f55da
Valeriia Sukhanova <122276487+AlphaVSpie@users.noreply.github.com>
2023-01-31 08:42:49 +0100

    added_subtask_5
    In this commit, I added subtask 5
    I couldn't stop thinking about the code, as there was time before the assignment was submitted, I made changes and improvements to the entire assignment

--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from pages.base_page import BasePage
 from pages.dashboard import Dashboard
 from pages.login_page import LoginPage
+from selenium.webdriver.common.by import By
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 
 
@@ -24,6 +25,9 @@ class TestLoginPage(unittest.TestCase):
     def test_log_in_to_the_system(self):
         user_login = LoginPage(self.driver)
         user_login.title_of_page()
+        scouts_panel = self.driver.find_element(By.XPATH, "//*[@id='__next']/form/div/div[1]/h5")
+        assert scouts_panel.text == 'Scouts Panel'
+        print("'Scouts Panel' title is fine")
         user_login.type_in_email("user01@getnada.com")
         user_login.type_in_password("Test-1234")
         user_login.click_on_the_sign_in_button()
